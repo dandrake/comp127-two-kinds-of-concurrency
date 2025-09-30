4a12743f6d30544ed43f08f7d3c97e4d6f404b35
Dan Drake <git@dandrake.org>
2025-09-30 05:46:24 -0500

    timer

--- src/paralleldanger/SisyphusStream.java
+++ src/paralleldanger/SisyphusStream.java
@@ -4,7 +4,16 @@ import java.util.stream.LongStream;
 
 public class SisyphusStream {
     public static void main(String[] args) {
-        LongStream.range(0, 100L)
-            .map(n -> 1);
+
+
+        Timer.measure(20, () -> {
+        SumCalculator calc = new SumCalculator();
+
+        LongStream.range(0, 10000000000L)
+            .map(n -> 1)
+            .forEach(calc::add);});
+
+        //System.out.println(calc.getTotal());
+        
     }
 }
